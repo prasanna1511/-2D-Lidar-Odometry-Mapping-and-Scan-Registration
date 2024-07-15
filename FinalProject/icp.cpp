0de0e4444276de12d67a03cbed8af45253d14b2e
prasanna1511 <53254596+prasanna1511@users.noreply.github.com>
2024-07-15 02:01:18 +0200

    Add files via upload

---
+++ FinalProject/icp.cpp
@@ -0,0 +1,10 @@
+#include "icp.hpp"
+#include <algorithm>
+#include <iostream>
+#include <open3d/Open3D.h>
+#include <open3d/core/ShapeUtil.h>
+#include <utility>
+#include <vector>
+#include "icp/icp.hpp"
+
+
---
+++ FinalProject/icp.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Eigen/Core>
+#include <unordered_map>
+#include <vector>
+#include <utility>
+#include <tuple>
+
+namespace icp {
+
+using PointCloud = std::vector<Eigen::Vector2d>;
+
+
+
+} // namespace icp
