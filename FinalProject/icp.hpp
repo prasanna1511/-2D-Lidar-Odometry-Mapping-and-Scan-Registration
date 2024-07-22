07b6e760d3f3fea3072d0570d4b4b05fc85bfe42
prasanna1511 <53254596+prasanna1511@users.noreply.github.com>
2024-07-22 10:49:39 +0200

    Update icp.hpp

--- FinalProject/icp.hpp
+++ FinalProject/icp.hpp
@@ -4,12 +4,24 @@
 #include <unordered_map>
 #include <vector>
 #include <utility>
-#include <tuple>
-
-namespace icp {
-
-using PointCloud = std::vector<Eigen::Vector2d>;
 
 
+Eigen::Vector2d centroid(const std::vector<Eigen::Vector2d> &points);
 
-} // namespace icp
+Eigen::Vector2d covariance(const std::vector<Eigen::Vector2d> &source_points, 
+                            const Eigen::Vector2d &source_mean, 
+                           const std::vector<Eigen::Vector2d> &target_points, 
+                           const Eigen::Vector2d &target_mean);
+
+Eigen::Matrix3d save_transformation(const Eigen::Matrix3d &old_transform, 
+                                    const Eigen::Matrix3d &new_transform);
+
+double error(const std::vector<Eigen::Vector2d> &source_points, 
+            const std::vector<Eigen::Vector2d> &target_points, 
+            const Eigen::Matrix3d &transform);
+
+std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>> neighbour_pixels(const Eigen::Vector2d &pixel, double range);
+
+std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>> GetAdjacentPixels(const Eigen::Vector2d &pixel, double range);
+
+std::vector<Eigen::Vector2d> pixel_points(const std::unordered_map<Eigen::Vector2d, std::vector<Eigen::Vector2d>> &target_grid, const Eigen::Vector2d &pixel);
