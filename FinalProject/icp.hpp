4595e599a2c2dc247bdfc0916d6eb2c4ac5aad78
prasanna1511 <53254596+prasanna1511@users.noreply.github.com>
2024-07-23 22:49:27 +0200

    Update icp.hpp

--- FinalProject/icp.hpp
+++ FinalProject/icp.hpp
@@ -4,7 +4,18 @@
 #include <unordered_map>
 #include <vector>
 #include <utility>
+#include <functional>
 
+namespace std {
+    template <>
+    struct hash<Eigen::Vector2d> {
+        std::size_t operator()(const Eigen::Vector2d &vec) const noexcept {
+            std::size_t h1 = std::hash<double>{}(vec.x());
+            std::size_t h2 = std::hash<double>{}(vec.y());
+            return h1 ^ (h2 << 1); // Combine the two hash values
+        }
+    };
+}
 
 Eigen::Vector2d centroid(const std::vector<Eigen::Vector2d> &points);
 
@@ -25,3 +36,28 @@ std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>> neighbour_pixels(const 
 std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>> GetAdjacentPixels(const Eigen::Vector2d &pixel, double range);
 
 std::vector<Eigen::Vector2d> pixel_points(const std::unordered_map<Eigen::Vector2d, std::vector<Eigen::Vector2d>> &target_grid, const Eigen::Vector2d &pixel);
+
+Eigen::Matrix3d icp(const std::vector<Eigen::Vector2d> &source_points, 
+                    const std::vector<Eigen::Vector2d> &target_points, 
+                    const Eigen::Matrix3d &initial_guess, 
+                    const std::size_t max_iterations, 
+                    const double tolerance);
+
+std::unordered_map<Eigen::Vector2d, std::vector<Eigen::Vector2d>> grid(const std::vector<Eigen::Vector2d> &points);
+
+std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>> k_near(const Eigen::Vector2d &pixel, double range);
+
+
+std::vector<Eigen::Vector2d> apply_transformation(const std::vector<Eigen::Vector2d> &points, 
+                                                const Eigen::Matrix3d &transform);
+
+std::vector<Eigen::Vector2d> concat_points(const std::vector<Eigen::Vector2d> &source_points, 
+                                                const std::vector<Eigen::Vector2d> &target_points);
+
+std::vector<Eigen::Vector2d> downsample(const std::vector<Eigen::Vector2d> &points, 
+                                                const std::size_t factor);
+
+std::vector<Eigen::Vector2d> downsample_mean(const std::vector<Eigen::Vector2d> &points,
+                                                 const std::size_t factor);
+
+
