e35c6bb97529c08be34b189f442ca27281c44c6e
prasanna1511 <53254596+prasanna1511@users.noreply.github.com>
2024-08-01 11:23:24 +0200

    Update icp.cpp

--- FinalProject/icp.cpp
+++ FinalProject/icp.cpp
@@ -7,85 +7,86 @@
 #include <Eigen/Dense>
 
 
-Eigen::Vector2d errorij(const Eigen::Vector2d &p, const Eigen::Vector2d &q, const Eigen::Matrix3d &T){
-    Eigen::Vector3d p_homogeneous(p.x(), p.y(), 1);
-    Eigen::Vector3d transformed_p_homo = (T* p_homogeneous);
-    double w = transformed_p_homo.z();
-    Eigen::Vector2d transformed_p;
-    if (w != 0.0){
-        transformed_p.x() = transformed_p_homo.x()/w;
-        transformed_p.y() = transformed_p_homo.y()/w;
-    }else{
-        transformed_p =Eigen::Vector2d::Zero();
+namespace icp {
+
+
+using PointCloud = std::vector<Eigen::Vector2d>;
+
+std::vector<Eigen::Vector2d> DownSample(const PointCloud &pcd, double voxel_size) {
+    std::unordered_map<int, Eigen::Vector2d> downsampled_vox;
+    for (const auto &point : pcd) {
+        int x = std::round(point[0] / voxel_size);
+        int y = std::round(point[1] / voxel_size);
+        int key = x * 73856093 + y* 19349663;
+        downsampled_vox[key] = point;
     }
-    return transformed_p - q;
-}
-
-
-Eigen::Matrix<double, 2, 3> Jacobian(const Eigen::Vector2d &p, const Eigen::Vector2d &q, const Eigen::Matrix3d &T){
-    Eigen::Vector3d p_homogeneous(p.x(), p.y(), 1);
-    Eigen::Vector3d transformed_p_homo = (T* p_homogeneous);
-    double w = transformed_p_homo.z();
-    Eigen::Vector2d transformed_p;
-    if (w != 0.0){
-        transformed_p.x() = transformed_p_homo.x()/w;
-        transformed_p.y() = transformed_p_homo.y()/w;
-    }else{
-        transformed_p =Eigen::Vector2d::Zero();
+    std::vector<Eigen::Vector2d> downsampled_pcd;
+    for (const auto &pair : downsampled_vox) {
+        downsampled_pcd.push_back(pair.second);
     }
-
-    double x = transformed_p.x();
-    double y = transformed_p.y();
-
-    Eigen::Matrix<double, 2, 3> J(2,3);
-    J << 1, 0, -y ,
-            0,1, x;
-    return J;
+    return downsampled_pcd;
 }
 
-void linear_system(const std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>> &correspondences,
-                    const Eigen::Matrix3d &T,
-                    Eigen::Matrix<double, 3, 3> &H,
-                    Eigen::Vector3d &b){
-    H.setZero();
-    b.setZero();
 
-for(const auto &corr :correspondences){
-    const  Eigen::Vector2d &p =corr.first;
-    const Eigen::Vector2d &q =corr.second;
-    Eigen::Vector2d e = errorij(p,q, T);
-    Eigen::MatrixXd J = Jacobian(p,q,T);
-    H += J.transpose()*J;
-    b += J.transpose()*e;
-}
- }
-
-
-Eigen::Vector3d delta_x(const Eigen::Matrix<double,3, 3> &H, const Eigen::Vector3d &b){
-    Eigen::LDLT<Eigen::Matrix<double,3,3>> ldlt(H);
-    if(ldlt.info()== Eigen::NumericalIssue){
-      std::cerr << "H is not positive definite or is singular." << std::endl;
-        return Eigen::Vector3d::Zero();   
+int nearestNeigbour(const Eigen::Vector2d &point, const PointCloud &ref_pcd) {
+    double min_dist = std::numeric_limits<double>::max();
+    int near_idx = -1;
+    for (int i = 0; i < ref_pcd.size(); i++) {
+        double dist = (point - ref_pcd[i]).norm();
+        if (dist < min_dist) {
+            min_dist = dist;
+            near_idx = i;
+        }
     }
-    return -ldlt.solve(b);
+    return near_idx;
+}
+
+Eigen::Vector2d Mean(const PointCloud &pcd) {
+    Eigen::Vector2d mean = Eigen::Vector2d::Zero();
+    for (const auto &point : pcd) {
+        mean += point;
+    }
+    return mean / pcd.size();
+}
+
+std::vector<Eigen::Vector2d> substractMean(const PointCloud &pcd, const Eigen::Vector2d &mean){
+    std::vector<Eigen::Vector2d> centered_pcd;
+    for (const auto &point : pcd) {
+    centered_pcd.push_back(point - mean);
+}
+    return centered_pcd;
+
+}
+
+Eigen::Matrix2d Covariance(const std::vector<Eigen::Vector2d>& p_prime, const std::vector<Eigen::Vector2d>& q_prime) {
+    Eigen::Matrix2d covariance = Eigen::Matrix2d::Zero();
+    for (int i = 0; i < p_prime.size(); i++) {
+        covariance += p_prime[i] * q_prime[i].transpose();
+    }
+    return covariance / p_prime.size();
+}
+
+Eigen::Matrix2d R(const Eigen::Matrix2d &covariance) {
+    Eigen::JacobiSVD<Eigen::Matrix2d> svd(covariance, Eigen::ComputeFullU | Eigen::ComputeFullV);
+    Eigen::Matrix2d U = svd.matrixU();
+    Eigen::Matrix2d V = svd.matrixV();
+    Eigen::Matrix2d R = V * U.transpose();
+    return R;
+}
+
+Eigen::Vector2d t(const Eigen::Vector2d &mean_pcd, const Eigen::Vector2d &mean_ref_pcd, const Eigen::Matrix2d &R) {
+    Eigen::Vector2d t = mean_ref_pcd - R * mean_pcd;
+    return t;
+}
+
+Eigen::Matrix3d ApplyTransformation(const Eigen::Matrix2d &R, const Eigen::Vector2d &t) {
+    Eigen::Matrix3d transformation = Eigen::Matrix3d::Identity();
+    transformation.topLeftCorner<2, 2>() = R;
+    transformation.topRightCorner<2, 1>() = t;
+    return transformation;
 }
 
 
-Eigen::Matrix3d apply_transformation(const Eigen::Vector3d &delta_x, const Eigen::Matrix3d &T){
-    double cos_theta = std::cos(delta_x.z());
-    double sin_theta = std::sin(delta_x.z());
-    Eigen::Matrix3d v2t;
-    v2t << cos_theta, -sin_theta, delta_x.x(),
-            sin_theta, cos_theta, delta_x.y(),
-            0, 0, 1;
-    return v2t*T;
-
-}
 
 
-
-
-
-
-
-
+} // namespace icp
