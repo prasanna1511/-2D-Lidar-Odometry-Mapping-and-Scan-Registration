30d75031881af1f59bc5b0fe3b39bf62a53be852
prasanna1511 <53254596+prasanna1511@users.noreply.github.com>
2024-08-01 11:24:08 +0200

    Update icp.hpp

--- FinalProject/icp.hpp
+++ FinalProject/icp.hpp
@@ -8,21 +8,31 @@
 #include <Eigen/Dense>
 
 
-Eigen::Vector2d errorij(const Eigen::Vector2d &p, const Eigen::Vector2d &q, const Eigen::Matrix3d &T);
+namespace icp {
+using PointCloud = std::vector<Eigen::Vector2d>;
+
+struct ICPResult{
+    std::vector<Eigen::Matrix3d> transformations;
+    std::vector<Eigen::Vector2d> final_pcd;
+};
+
+std::vector<Eigen::Vector2d> DownSample(const PointCloud &pcd, double voxel_size);
+
+int nearestNeigbour(const Eigen::Vector2d &point, const PointCloud &ref_pcd);
+
+Eigen::Vector2d Mean(const PointCloud &pcd);
+
+std::vector<Eigen::Vector2d> substractMean(const PointCloud &pcd, const Eigen::Vector2d &mean);
+
+Eigen::Matrix2d Covariance(const std::vector<Eigen::Matrix<double, 2, 1>>& p_prime, const std::vector<Eigen::Matrix<double, 2, 1>>& q_prime);
+
+Eigen::Matrix2d R(const Eigen::Matrix2d &covariance);
+
+Eigen::Vector2d t(const Eigen::Vector2d &mean_pcd, const Eigen::Vector2d &mean_ref_pcd, const Eigen::Matrix2d &R);
+
+Eigen::Matrix3d ApplyTransformation(const Eigen::Matrix2d &R, const Eigen::Vector2d &t);
+
+ICPResult icp(const std::vector<Eigen::Vector2d> &pcds, double voxel_size, int max_iter, double tolerance);
 
 
-Eigen::Matrix<double, 2, 3> Jacobian(const Eigen::Vector2d &p, const Eigen::Vector2d &q, const Eigen::Matrix3d &T);
-
-
-void linear_system(const std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>> &correspondences,
-                    const Eigen::Matrix3d &T,
-                    Eigen::Matrix<double, 3, 3> &H,
-                    Eigen::Vector3d &b);
-
-Eigen::Vector3d delta_x(const Eigen::Matrix<double,3, 3> &H, const Eigen::Vector3d &b);
-
-Eigen::Matrix3d apply_transformation(const Eigen::Vector3d &delta_x, const Eigen::Matrix3d &T);
-
-
-
-
+}// namespace icp
